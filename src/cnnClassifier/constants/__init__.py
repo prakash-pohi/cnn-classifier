520bcedf1efbe62420203ca1c95e1f8461abb05c
prakash-pohi <prakashpohi3@gmail.com>
2024-02-17 11:45:32 +0530

    evaluation and DVC

--- src/cnnClassifier/constants/__init__.py
+++ src/cnnClassifier/constants/__init__.py
@@ -1,4 +1,4 @@
 from pathlib import Path
 
-CONFIG_FILE_PATH = Path("config/config.yaml")
-PARAMS_FILE_PATH = Path("params.yaml")
+CONFIG_FILE_PATH = Path("/Users/ppohi/Desktop/all_python_projects/DL_project/cnn-classifier/config/config.yaml")
+PARAMS_FILE_PATH = Path("/Users/ppohi/Desktop/all_python_projects/DL_project/cnn-classifier/params.yaml")
